11cceed9bfdd5260cf9d58a409e62079138de886
jacks wrld <153364853+Jackswrld@users.noreply.github.com>
2026-08-04 11:23:55 +0100

    feat: wire relations, characters, and staff sections into Detail page

--- src/Detailpg/Detail.jsx
+++ src/Detailpg/Detail.jsx
@@ -4,6 +4,9 @@ import detailFixture from "./detailFixture";
 import DetailBanner from "./components/DetailBanner";
 import DetailSidebar from "./components/DetailSidebar";
 import DetailHeader from "./components/DetailHeader";
+import DetailRelations from "./components/DetailRelations";
+import DetailCharacters from "./components/DetailCharacters";
+import DetailStaff from "./components/DetailStaff";
 
 const Detail = () => {
   const { id } = useParams();
@@ -18,6 +21,9 @@ const Detail = () => {
 
         <div className="detail-main">
           <DetailHeader media={media} />
+          <DetailRelations relations={media.relations} />
+          <DetailCharacters characters={media.characters} />
+          <DetailStaff staff={media.staff} />
         </div>
       </div>
     </div>
